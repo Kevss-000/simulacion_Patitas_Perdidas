8a7e2dcb57a47eb0026df22f95203b0e5a5cb663
jesus barrios <jbarriosrodriguez083@gmail.com>
2026-04-28 19:21:08 -0500

    feat se crea limpieza y simulacion de la tabla ubicaciones

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from utils.simulacion_usuarios import generar_simulacion
 from notebook.limpieza_usuarios import limpiar_simulasiones
+from notebook.limpieza_ubicaciones import limpiar_simulaciones
+from utils.simulacion_ubicaciones import generar_ubicaciones
 
 simulaciones = generar_simulacion(20)
 
